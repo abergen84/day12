a820fb261db34a5e77adfdc7481284204e0206bb
Adam Bergen <bergen@gmail.com>
2014-10-08 16:30:16 -0500

    latest push

--- hw12.js
+++ hw12.js
@@ -8,97 +8,118 @@
 //Problem 1
 
 //Basic
-function sum(a, b){
-	"use strict";
-	
-	return a + b;
+function sum(a, b) {
+    "use strict";
+
+    return a + b;
 }
 
 //For loop
+function sumForLoop() {
+    "use strict";
 
-function sumForLoop(){
-	"use strict";
-	
-	var args = Array.prototype.slice.apply(arguments, []), 
-	sum = 0;
-	
-	for(var i = 0; i < args.length; i++){
-		sum += args[i];
-	}
-	return sum;
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
+
+    for (var i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
 }
 
 //ForEach 
+function sumForEach() {
+    "use strict";
 
-function sumForEach(){
-	"use strict";
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
 
-	var args = Array.prototype.slice.apply(arguments, []),
-	sum = 0;
-
-	args.forEach(function(num){ sum += num; });
-	return sum;
+    args.forEach(function(num) {
+        sum += num;
+    });
+    return sum;
 
 }
 
 //Custom ForEach
+function forEach(array, callback) {
+    "use strict";
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i], i);
+    }
+}
 
-function sumForEachCustom(){
-	"use strict";
-	
-	var args = Array.prototype.slice.apply(arguments, []),
-	sum = 0;
+function sumForEachCustom() {
+    "use strict";
 
-	forEach(args, function(){ sum += arguments[0]; });
-	return sum;
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
+
+    forEach(args, function() {
+        sum += arguments[0];
+    })
+    return sum;
 }
 
 // Problem 2
 
 // for loop
+function averageForLoop() {
+    "use strict";
 
-function averageForLoop(){
-	"use strict";
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
 
-	var args = Array.prototype.slice.apply(arguments, []),
-	sum = 0;
-
-	for(var i = 0; i < args.length; i++){
-		sum += args[i];
-	}
-	return (sum/args.length);
+    for (var i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return (sum / args.length);
 }
 
 //ForEach loop
+function averageForEach() {
+    "use strict";
 
-function averageForEach(){
-	"use strict";
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
 
-	var args = Array.prototype.slice.apply(arguments, []),
-	sum = 0;
-
-	args.forEach(function(num){ sum += num; });
-	return (sum/args.length);
+    args.forEach(function(num) {
+        sum += num;
+    });
+    return (sum / args.length);
 }
 
 //Custom ForEach
-//
-//
+function averageForEachCustom() {
+    "use strict";
+
+    var args = Array.prototype.slice.apply(arguments, []),
+        sum = 0;
+
+    forEach(args, function() {
+        sum += arguments[0];
+    });
+    return (sum / args.length);
+}
 
 //Problem 3
 
 // For Loop
-// 
-function largestForLoop(){
-	"use strict";
+function largestForLoop() {
+    "use strict";
 
-	var args = Array.prototype.slice.apply(arguments, []),
-	sum = 0;
+    var args = Array.prototype.slice.apply(arguments, []),
+        largestNumber = 0;
 
-	for(var i = 0; i < args.length; i++){
-//trying to figure out what it would do here		
-	}
-//	return 
+    for (var i = 0; i < args.length; i++) {
+        // if args[i] is more than largestNumber
+	        // set largestNumber to args[i]        
+        if(args[i] > largestNumber){
+        	
+        	largestNumber = args[i];
+        } 
+    }
+	return largestNumber;
 }
 
 
@@ -107,19 +128,57 @@ function largestForLoop(){
 //
 
 //for Loop
-function longestForLoop(){
-	"use strict";
+function longestForLoop() {
+    "use strict";
 
-	var args = Array.prototype.slice.apply(arguments, []);
+    var args = Array.prototype.slice.apply(arguments, []);
 
-	for(var i = 0; i < args.length; i++){
-		return args.length;
-	}
-		return args.length[0];
+    for (var i = 0; i < args.length; i++) {
+        return args.length;
+    }
+    return args.length[0];
 }
 
 
+//Problem 5
 
+// function forEach(array, callback){
+//     "use strict";
+//     for(var i = 0; i < array.length; i++){
+//         callback(array[i], i);
+//     }
+// }
 
+// //The Date Sorting Function
+// //
+// function sortDateForLoop(dates){
+// 	var newArray = [],
+// 		earliestDate = dates[0];
 
+// 	for(var i = 0; i < dates.length; i++){
+// 		//find the lowest date and put it at the front of newArray
+// 		for(var j = 0; j < dates.length; j++){
 
+// 			if(+dates[j] < +earliestDate){
+// 				earliestDate = dates[j]
+// 			}
+
+// //			earliestDate = dates[j].getTime() < earliestDate.getTime() ? dates[j] : 
+// //				earliestDate;
+// 		}
+// 	}
+// }
+
+// function sortDateForEach(){}
+
+// function sortDateCustomForEach(){}
+
+// var dates = [
+// 	new Date(2014, 9, 4)
+// 	new Date(2014, 9, 3)
+// 	new Date(2014, 8, 30)
+// 	new Date(2012, 9, 1)
+// 	new Date(2010, 2, 13)
+// 	];
+
+// console.log(dates);
